795ad12ff5f758ed4740800ea172914fd3d49675
Buse Boylan <buse.boylan@santsg.com>
2023-01-14 21:30:09 +0300

    changed

---
+++ TravelArt/TravelArt/Controllers/GezilecekYerlerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelArt.Controllers
+{
+    public class GezilecekYerlerController : Controller
+    {
+        public IActionResult GezilecekYerlerIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelArt/TravelArt/Controllers/LogInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelArt.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult LogInIndex()
+        {
+            return View();
+        }
+    }
+}
